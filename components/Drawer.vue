714623389b115bf9b588dbf74aff64194d37646d
Mei <74983120+meixsu@users.noreply.github.com>
2022-12-12 12:06:51 +0800

    created Drawer component

---
+++ components/Drawer.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="drawer-container" :class="showMenu ? 'w-full' : 'w-0'">
+    <div
+      class="drawer-overlay"
+      @click="toggleMenu"
+      :class="showMenu ? 'w-full opacity-100' : 'w-0 p-0 opacity-0'"
+    ></div>
+    <div class="drawer" :class="showMenu ? 'w-[300px]' : 'w-0 p-0'">
+      <button
+        @click="toggleMenu"
+        class="drawer-close"
+        :class="showMenu ? 'block' : 'hidden'"
+      >
+        <span class="sr-only">Close menu</span>
+        <img src="~/assets/images/icon-close.svg" alt="Close" />
+      </button>
+      <nav class="drawer-menu" :class="showMenu ? 'flex' : 'hidden'">
+        <div v-for="item in menu" :key="item">
+          <a href="#">{{ item }}</a>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  showMenu: {
+    type: Boolean,
+    required: true,
+  },
+  toggleMenu: {
+    type: Function,
+    required: true,
+  },
+  menu: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,7 @@
   <header>
     <div class="header">
       <div class="drawer-open">
-        <button @click="showMenu = true">
+        <button @click="toggleMenu">
           <img src="~/assets/images/icon-menu.svg" alt="Menu" />
         </button>
       </div>
@@ -12,34 +12,15 @@
         </div>
       </div>
       <div class="header-end">
-        <button><img src="~/assets/images/icon-cart.svg" alt="Cart" /></button>
+        <button>
+          <img src="~/assets/images/icon-cart.svg" alt="Cart" />
+        </button>
         <button>
           <img src="~/assets/images/image-avatar.png" alt="User" class="w-10" />
         </button>
       </div>
     </div>
-    <div class="drawer-container" :class="showMenu ? 'w-full' : 'w-0'">
-      <div
-        class="drawer-overlay"
-        @click="showMenu = false"
-        :class="showMenu ? 'w-full opacity-100' : 'w-0 p-0 opacity-0'"
-      ></div>
-      <div class="drawer" :class="showMenu ? 'w-[300px]' : 'w-0 p-0'">
-        <button
-          @click="showMenu = false"
-          class="drawer-close"
-          :class="showMenu ? 'block' : 'hidden'"
-        >
-          <span class="sr-only">Close menu</span>
-          <img src="~/assets/images/icon-close.svg" alt="Close" />
-        </button>
-        <nav class="drawer-menu" :class="showMenu ? 'flex' : 'hidden'">
-          <div v-for="item in menu" :key="item">
-            <a href="#">{{ item }}</a>
-          </div>
-        </nav>
-      </div>
-    </div>
+    <Drawer :showMenu="showMenu" :toggleMenu="toggleMenu" :menu="menu" />
   </header>
 </template>
 
@@ -47,4 +28,8 @@
 const menu = ["Collections", "Men", "Women", "About", "Contact"];
 
 const showMenu = ref(false);
+
+function toggleMenu() {
+  showMenu.value = !showMenu.value;
+}
 </script>
