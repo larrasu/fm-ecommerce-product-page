e10c35cc948769c119d07a3ad11b9a1aba0310cd
Mei <74983120+meixsu@users.noreply.github.com>
2022-12-12 11:56:52 +0800

    drawer created

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,15 @@
 <template>
   <header>
     <div class="header">
-      <div>
+      <div class="drawer-open">
         <button @click="showMenu = true">
           <img src="~/assets/images/icon-menu.svg" alt="Menu" />
         </button>
       </div>
       <div>
-        <div><img src="~/assets/images/logo.svg" alt="Sneakers Logo" /></div>
+        <div>
+          <img src="~/assets/images/logo.svg" alt="Sneakers Logo" />
+        </div>
       </div>
       <div class="header-end">
         <button><img src="~/assets/images/icon-cart.svg" alt="Cart" /></button>
@@ -16,18 +18,25 @@
         </button>
       </div>
     </div>
-    <div class="drawer-backdrop" :class="showMenu ? 'w-full' : 'w-0'">
-      <div class="drawer" :class="showMenu ? 'w-full' : 'w-0'">
-        <button @click="showMenu = false">
+    <div class="drawer-container" :class="showMenu ? 'w-full' : 'w-0'">
+      <div
+        class="drawer-overlay"
+        @click="showMenu = false"
+        :class="showMenu ? 'w-full opacity-100' : 'w-0 p-0 opacity-0'"
+      ></div>
+      <div class="drawer" :class="showMenu ? 'w-[300px]' : 'w-0 p-0'">
+        <button
+          @click="showMenu = false"
+          class="drawer-close"
+          :class="showMenu ? 'block' : 'hidden'"
+        >
           <span class="sr-only">Close menu</span>
           <img src="~/assets/images/icon-close.svg" alt="Close" />
         </button>
-        <nav>
-          <ul>
-            <li v-for="item in menu" :key="item">
-              <a href="#">{{ item }}</a>
-            </li>
-          </ul>
+        <nav class="drawer-menu" :class="showMenu ? 'flex' : 'hidden'">
+          <div v-for="item in menu" :key="item">
+            <a href="#">{{ item }}</a>
+          </div>
         </nav>
       </div>
     </div>
