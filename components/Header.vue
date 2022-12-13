350dbc9fe6d7b6f17e0306fa602138e1d3956b2b
Mei <74983120+meixsu@users.noreply.github.com>
2022-12-13 13:54:49 +0800

    update

--- components/Header.vue
+++ components/Header.vue
@@ -11,8 +11,19 @@
           <img src="~/assets/images/logo.svg" alt="Sneakers Logo" />
         </div>
       </div>
+      <nav class="header-menu">
+        <div v-for="item in menu" :key="item" class="menu-item">
+          <a href="#">{{ item }}</a>
+        </div>
+      </nav>
       <div class="header-end">
-        <button @click="toggleCart" class="group">
+        <button @click="toggleCart" class="group relative">
+          <div
+            v-for="item of cart"
+            class="bg-primary-100 text-white px-2 text-xs absolute -right-2 rounded-full -top-2"
+          >
+            {{ item.qty }}
+          </div>
           <svg width="22" height="20" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
@@ -23,7 +34,9 @@
             />
           </svg>
         </button>
-        <button>
+        <button
+          class="transition border-2 hover:border-primary-100 rounded-full"
+        >
           <img src="~/assets/images/image-avatar.png" alt="User" class="w-10" />
         </button>
       </div>
@@ -38,15 +51,36 @@
       <div
         class="cart"
         :class="
-          showCart ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-1'
+          showCart
+            ? 'opacity-100 translate-y-0'
+            : 'opacity-0 translate-y-1 hidden'
         "
       >
         <div class="cart-heading"><h1>Cart</h1></div>
-        <div
-          class="cart-body"
-          :class="cart != '' ? '' : 'flex items-center justify-center'"
-        >
-          <div class="empty">Your cart is empty.</div>
+        <div class="cart-body">
+          <div v-if="cart == ''" class="empty">Your cart is empty.</div>
+          <div v-if="cart" v-for="item of cart" class="flex gap-4 mb-4">
+            <img
+              src="/images/image-product-1.jpg"
+              alt=""
+              class="w-14 h-14 object-cover rounded-lg"
+            />
+            <div class="mr-auto">
+              <h1>{{ item.name }}</h1>
+              <p>
+                {{ formatter.format(item.price) }} x {{ item.qty }}
+                <span class="font-bold ml-2">
+                  {{ formatter.format(item.price * item.qty) }}
+                </span>
+              </p>
+            </div>
+            <div>
+              <button @click="$emit('deleteItem', item.id)">
+                <img src="~/assets/images/icon-delete.svg" alt="" />
+              </button>
+            </div>
+          </div>
+          <button v-if="cart != ''" class="btn-primary">Checkout</button>
         </div>
       </div>
     </div>
@@ -57,9 +91,15 @@
 
 <script setup>
 const props = defineProps(["cart", "showCart", "toggleCart"]);
+const emit = defineEmits(["deleteItem"]);
 const menu = ["Collections", "Men", "Women", "About", "Contact"];
 
 const showMenu = ref(false);
+
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
 
 function toggleMenu() {
   showMenu.value = !showMenu.value;
--- components/Lightbox.vue
+++ components/Lightbox.vue
@@ -10,20 +10,25 @@
       v-for="(img, index) in images"
       :key="index"
       :src="img.img"
-      class="md:rounded-lg object-cover w-full transition-opacity duration-500 ease-in-out"
-      :class="
-        currentImg === index
-          ? 'h-[300px] md:max-h-full opacity-100'
-          : 'h-0 opacity-0'
-      "
+      class="object-cover w-full transition-opacity duration-500 ease-in-out md:hidden"
+      :class="currentImg === index ? 'h-[300px] opacity-100' : 'h-0 opacity-0'"
       alt="Preview"
     />
-    <div class="md:grid hidden grid-cols-4 gap-4 mt-4">
+    <img
+      v-for="(img, index) in images"
+      :key="index"
+      :src="img.img"
+      @click="toggleModal"
+      class="rounded-xl object-cover w-full transition-opacity duration-500 ease-in-out cursor-pointer hidden md:block"
+      :class="currentImg === index ? 'h-[500px] opacity-100' : 'h-0 opacity-0'"
+      alt="Preview"
+    />
+    <div class="md:grid hidden grid-cols-4 gap-4 mt-4 md:mt-8 md:gap-8">
       <button
         @click="changeImg(index)"
         v-for="(img, index) in images"
         :key="index"
-        class="overflow-hidden transition border-2 rounded-lg"
+        class="overflow-hidden transition border-2 rounded-xl"
         :class="
           currentImg == index ? 'border-primary-100' : 'border-transparent'
         "
@@ -35,6 +40,66 @@
           :class="currentImg === index ? 'opacity-50' : 'opacity-100'"
         />
       </button>
+    </div>
+
+    <div
+      class="lightbox-modal"
+      :class="showModal ? 'opacity-100' : 'opacity-0 -z-50'"
+    >
+      <div class="lightbox-overlay" @click="toggleModal"></div>
+
+      <div class="lightbox-content">
+        <div class="lightbox-image">
+          <button @click="showModal = false" class="float-right">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              class="fill-white w-8 h-8 mb-4 -mr-2"
+            >
+              <path
+                d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </button>
+          <button @click="prev" class="prev">
+            <img src="~/assets/images/icon-previous.svg" alt="Previous" />
+          </button>
+          <button @click="next" class="next">
+            <img src="~/assets/images/icon-next.svg" alt="Next" />
+          </button>
+          <img
+            v-for="(img, index) in images"
+            :key="index"
+            :src="img.img"
+            class="rounded-xl object-cover w-full transition-opacity duration-500 ease-in-out"
+            :class="
+              currentImg === index ? 'h-[500px] opacity-100' : 'h-0 opacity-0'
+            "
+            alt="Preview"
+          />
+        </div>
+        <div class="md:grid hidden grid-cols-4 gap-4 mt-4 md:mt-8 md:gap-8">
+          <button
+            @click="changeImg(index)"
+            v-for="(img, index) in images"
+            :key="index"
+            class="overflow-hidden transition border-2 rounded-xl"
+            :class="
+              currentImg == index ? 'border-primary-100' : 'border-transparent'
+            "
+          >
+            <img
+              :src="img.thumb"
+              alt="Preview"
+              class="hover:opacity-50 transition"
+              :class="currentImg === index ? 'opacity-50' : 'opacity-100'"
+            />
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -59,6 +124,8 @@ const images = ref([
     thumb: "/images/image-product-4-thumbnail.jpg",
   },
 ]);
+const showModal = ref(false);
+
 function prev() {
   if (currentImg.value === 0) {
     currentImg.value = images.value.length - 1;
@@ -75,5 +142,9 @@ function next() {
 }
 function changeImg(index) {
   currentImg.value = index;
+}
+
+function toggleModal() {
+  showModal.value = !showModal.value;
 }
 </script>
